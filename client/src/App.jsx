f6d5de46a3f43c25bef6c99090dbded239e734da
Anupam Singh <anupamsingh584210@gmail.com>
2024-08-03 02:25:53 +0530

    Footer is Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from "./pages/Home";
 import About from "./pages/About"
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
+import './App.css';
 export default function App() {
   return (
     <div>
@@ -16,6 +18,8 @@ export default function App() {
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/signin" element={<Signin />}></Route>
         </Routes>
+
+        <Footer />
       </BrowserRouter>
    
          
