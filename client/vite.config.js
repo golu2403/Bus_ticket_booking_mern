61006e54b503960a3170eaa260f434dce70c9116
Amit kumar singh <131990860+golu2403@users.noreply.github.com>
2025-02-11 01:00:12 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Ensures correct base path for production
+  server: {
+    historyApiFallback: true
+  }
 });
+
